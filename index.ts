472bfd5ba64f5c76429ff9983edb96dec293010d
JohnnyLin-a <57846020+JohnnyLin-a@users.noreply.github.com>
2022-06-27 04:13:48 +0000

    Do not trigger exception when axios get fails

--- index.ts
+++ index.ts
@@ -30,12 +30,19 @@ try {
     for (let url of config.urls) {
         await setTimeout(3000)
         // Get all posts
-        const response = await axios.get(url, {
-            headers: {
-                "User-Agent":
-                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36",
-            },
-        })
+        let response: any
+        try {
+            response = await axios.get(url, {
+                headers: {
+                    "User-Agent":
+                        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36",
+                },
+            })
+        } catch (e) {
+            console.log("Failed to get ", url)
+            // Personally want it to not do anything when it fails, feel free to change this.
+            continue
+        }
 
         const $ = cheerio.load(response.data)
         const allPosts: string[] = []
